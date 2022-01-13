880ab76fc51b5d4a062cf9d6066c9958a21c160f
Martín Lombardo <to@mlomb.me>
2022-01-13 15:34:47 -0300

    use system Redis

--- tests/redis.ts
+++ tests/redis.ts
@@ -1,10 +1,6 @@
 import Redis from "ioredis";
 
-let rc = new Redis({
-    host: process.env.REDIS_HOST || "localhost",
-    port: parseInt(process.env.REDIS_PORT || "6379"),
-    db: 15,
-});
+let rc = new Redis({ db: 15 });
 
 beforeEach((done) => {
     rc.flushdb(done);
