98251915a2746d3a64fca3bf2e706908356b9ecf
Martín Lombardo <to@mlomb.me>
2022-01-13 15:08:58 -0300

    fix ci

--- tests/redis.ts
+++ tests/redis.ts
@@ -1,6 +1,10 @@
-import Redis from 'ioredis';
+import Redis from "ioredis";
 
-let rc = new Redis({ db: 15 });
+let rc = new Redis({
+    host: process.env.REDIS_HOST || "localhost",
+    port: parseInt(process.env.REDIS_PORT || "6379"),
+    db: 15,
+});
 
 beforeEach((done) => {
     rc.flushdb(done);
